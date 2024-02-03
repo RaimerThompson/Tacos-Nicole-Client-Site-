f2773729f70c9d7f07440dae3bceb9e295987633
Raimer Thompson <rthomp22@gmail.com>
2024-02-03 14:44:23 -0500

    menu functionality added to links hamburger menu

--- js/mobile.js
+++ js/mobile.js
@@ -1,9 +1,11 @@
 const menuBtn = document.querySelector('.hamburger__btn');
 const mobileNav = document.getElementById('navbar');
+const mobileLinks = document.querySelectorAll('.header__link')
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
-  if (!menuOpen) {
+  menuOpen = !menuOpen
+  if (menuOpen) {
     menuBtn.classList.add('close');
     menuOpen = true;
     mobileNav.style.display = 'flex';
@@ -16,4 +18,12 @@ menuBtn.addEventListener('click', () => {
     mobileNav.classList.remove('active');
    }
 })
-;
+
+mobileLinks.forEach(Link => {
+  Link.addEventListener('click', () => {
+    menuOpen = false;
+    mobileNav.style.display = 'flex';
+    mobileNav.classList.remove('active');
+  })
+})
+
